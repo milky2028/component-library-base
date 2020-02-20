75f9302abaedd21bfd0de8c21029202897e1f8c1
Tyler <tgross@skyroad.com>
2020-02-20 12:11:38 -0600

    remvoe comments

--- src/AvHeader/index.tsx
+++ src/AvHeader/index.tsx
@@ -1,13 +1,4 @@
-import { h } from 'preact';
-
-// TypeScript ensures consistent prop types across components
-// and simplifies interfaces so that teams injesting components
-// have a clear understanding of what the component needs/uses.
-
-// Prefer/necessitate functional components and hooks.
-// Class methods do not minify well and can lead to larger network payloads.
 export default function AvHeader(content: string) {
-  // Prefer CSS in JS.
   const styles = {
     header: {
       fontSize: '32px',
